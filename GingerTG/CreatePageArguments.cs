eca28e91bd34bbb88dc620aaedcda3884e020922
Victor <publicdomainvic@gmail.com>
2022-04-06 10:32:30 -0400

    Add createPage

---
+++ GingerTG/CreatePageArguments.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace GingerTG
+{
+    /// <summary>
+    /// Create page arguments.
+    /// </summary>
+    public class CreatePageArguments
+    {
+        public string ShortName { get; set; }
+        public string AccessToken { get; set; }
+        public string Title { get; set; }
+        public string AuthorName { get; set; }
+        public string AuthorUrl { get; set; }
+        public string Content { get; set; }
+        public bool ReturnContent { get; set; }
+    }
+}
--- GingerTG/Program.cs
+++ GingerTG/Program.cs
@@ -4,10 +4,12 @@
 // </copyright>
 
 using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
 using Fclp;
 using Telegraph.Net;
+using Telegraph.Net.Models;
 
 namespace GingerTG
 {
@@ -26,7 +28,7 @@ namespace GingerTG
 
             // Action
             p.Setup<string>('a', "action")
-             .Callback(action => ProcessAction(action, args))
+             .Callback(action => ProcessActionAsync(action, args))
              .Required();
 
             // Parse
@@ -45,7 +47,7 @@ namespace GingerTG
         /// </summary>
         /// <param name="action">Action.</param>
         /// <param name="args">Arguments.</param>
-        private static void ProcessAction(string action, string[] args)
+        private static async void ProcessActionAsync(string action, string[] args)
         {
             // Process implemented methods
             switch (action)
@@ -140,7 +142,132 @@ namespace GingerTG
                 // Create page
                 case "createPage":
 
-                    // TODO Create page
+                    /* Parse arguments */
+
+                    // Fluent
+                    var createPageParser = new FluentCommandLineParser<CreatePageArguments>();
+
+                    // short_name
+                    createPageParser.Setup(arg => arg.ShortName)
+                        .As('s', "short-name");
+
+                    // access_token
+                    createPageParser.Setup(arg => arg.AccessToken)
+                        .As('k', "access-token")
+                        .Required();
+
+                    // title
+                    createPageParser.Setup(arg => arg.Title)
+                        .As('t', "title")
+                        .Required();
+
+                    // author_name
+                    createPageParser.Setup(arg => arg.AuthorName)
+                        .As('n', "author-name");
+
+                    // author_url
+                    createPageParser.Setup(arg => arg.AuthorUrl)
+                        .As('u', "author-url");
+
+                    // content
+                    createPageParser.Setup(arg => arg.Content)
+                        .As('c', "content")
+                        .Required();
+
+                    // return_content
+                    createPageParser.Setup(arg => arg.ReturnContent)
+                        .As('r', "return-content");
+
+                    // Parse
+                    var createPageParserResult = createPageParser.Parse(args);
+
+                    // Check for errors
+                    if (createPageParserResult.HasErrors)
+                    {
+                        // Advise user
+                        Console.WriteLine(createPageParserResult.ErrorText);
+
+                        // Halt flow
+                        return;
+                    }
+
+                    /* Create page */
+
+                    // Advise user
+                    Console.WriteLine("Creating page...");
+
+                    try
+                    {
+                        // Set telegraph client
+                        var client = new TelegraphClient();
+
+                        // Set token client
+                        ITokenClient tokenClient = client.GetTokenClient(createPageParser.Object.AccessToken);
+
+                        // Set nodes
+                        var nodes = new List<NodeElement>();
+                        nodes.Add(
+                            new NodeElement("p",
+                                null,
+                                createPageParser.Object.Content,
+                                null
+                            )
+                        );
+
+                        // Set content
+                        NodeElement[] content = nodes.ToArray();
+
+                        // Create page
+                        Page page = await tokenClient.CreatePageAsync(
+                          createPageParser.Object.Title,
+                          content,
+                          createPageParser.Object.AuthorName,
+                          createPageParser.Object.AuthorUrl,
+                          createPageParser.Object.ReturnContent
+                        );
+
+                        // Check if OK
+                        if (page.Path.Length > 0)
+                        {
+                            // Set page data
+                            string pageData =
+                            $"path: {page.Path}{Environment.NewLine}" +
+                            $"url: {page.Url}{Environment.NewLine}" +
+                            $"title: {page.Title}{Environment.NewLine}" +
+                            $"description: {page.Description}{Environment.NewLine}" +
+                            $"author_name: {page.AuthorName}{Environment.NewLine}" +
+                            $"author_url: {page.AuthorUrl}{Environment.NewLine}" +
+                            $"image_url: {page.ImageUrl}{Environment.NewLine}" +
+                            $"content: {page.Content.ToString()}{Environment.NewLine}" +
+                            $"views: {page.Views}{Environment.NewLine}" +
+                            $"can_edit: {page.CanEdit}{Environment.NewLine}";
+
+                            // Set message
+                            string createPageMessage = $"{Environment.NewLine}{Environment.NewLine}{DateTime.Now}{Environment.NewLine}{pageData}";
+
+                            // Save page data to disk
+                            File.AppendAllText("GingerTG-Success.txt", createPageMessage);
+
+                            // Advise user
+                            Console.WriteLine($"Page created.{pageData}");
+                        }
+                        else
+                        {
+                            // Throw exception with error message
+                            throw new Exception("No page path returned.");
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        // Advise user
+                        Console.WriteLine($"Create page error: {ex.Message}");
+
+                        // Log to file
+                        File.AppendAllText("GingerTG-ErrorLog.txt", $"{Environment.NewLine}{Environment.NewLine}{DateTime.Now}{Environment.NewLine}Create page error: { ex.Message}");
+
+                        // Halt flow
+                        return;
+                    }
 
                     break;
 
